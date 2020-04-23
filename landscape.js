7a3d426412d898e0d23ed46f649c8eebd2a3b557
Kevin Horning <kevinsemailaddress123@gmail.com>
2020-04-23 10:31:54 -0400

    started landscape

--- landscape.js
+++ landscape.js
@@ -1,27 +1,26 @@
-var imagesClass0 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass1 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass2 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass3 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass4 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass5 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass6 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass7 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass8 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass9 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imagesClass10 = ["imgHorz1.JPG", "imgHorz2.JPG", "imgHorz3.JPG", "imgAbout.JPG"];
-var imageArray = [imagesClass0, imagesClass1, imagesClass2, imagesClass3, imagesClass4, imagesClass5, imagesClass6, imagesClass7, imagesClass8, imagesClass9, imagesClass10];
+var imagesClass0 = ["photos/BOA.jpg"];
+var imagesClass1 = ["photos/imgHorz1.JPG"];
+var imagesClass2 = ["photos/atlskyline.jpg"];
+var imagesClass3 = ["photos/25park.jpg"];
+var imagesClass4 = ["photos/teleskyline.jpg"];
+var imagesClass5 = ["photos/moon.jpg"];
+var imagesClass6 = ["imgHorz2.JPG"];
+var imagesClass7 = ["imgHorz2.JPG"];
+var imagesClass8 = ["imgHorz2.JPG"];
+var imagesClass9 = ["imgHorz2.JPG"];
+var imageArray = [imagesClass0, imagesClass1, imagesClass2, imagesClass3, imagesClass4, imagesClass5, imagesClass6, imagesClass7, imagesClass8, imagesClass9];
 document.getElementById("shuffle").addEventListener("click", function(){ shuffle(); } );
 
 function shuffle(){    
     numberOfSections = document.getElementsByClassName("imgSection").length;
-    selectedArray = [[], [], [], [], [], [], [], [], [], [], []];
+    selectedArray = [[], [], [], [], [], [], [], [], [], []];
     for (sectionNumber = 0; sectionNumber < numberOfSections; sectionNumber++){
-        for (imgClass = 0; imgClass < 11; imgClass++){  
+        for (imgClass = 0; imgClass < 10; imgClass++){  
             randomIndex = Math.random() * imageArray[imgClass].length | 0;
             selectedImage = imageArray[imgClass][randomIndex];
             selectedArray[imgClass].push(selectedImage);
             imageArray[imgClass].splice(randomIndex, 1);
-            containerIndex = imgClass + (sectionNumber * 11);
+            containerIndex = imgClass + (sectionNumber * 10);
             document.getElementById("img" + containerIndex).src = selectedImage;
         }
     }
@@ -37,6 +36,5 @@ function shuffle(){
         imagesClass7.push(selectedArray[7][sectionNumber]);
         imagesClass8.push(selectedArray[8][sectionNumber]);
         imagesClass9.push(selectedArray[9][sectionNumber]);
-        imagesClass10.push(selectedArray[10][sectionNumber]);
     }
 }
